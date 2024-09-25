ba059d89815480eb95189c3ea69063f8b6d56585
Jon Wissing <jon.wissing@alexandra.dk>
2024-09-25 10:45:51 +0200

    Add keybindings symlinking to setup script

--- symlink_settings.sh
+++ symlink_settings.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 
 SETTINGS_PATH="$HOME/.config/Code/User/settings.json"
-BACKUP_PATH="$HOME/.config/Code/User/settings_backup.json"
+BACKUP_SETTINGS_PATH="$HOME/.config/Code/User/settings_backup.json"
+
+KEYBINDINGS_PATH="$HOME/.config/Code/User/keybindings.json"
+BACKUP_KEYBINDINGS_PATH="$HOME/.config/Code/User/keybindings_backup.json"
+
 SCRIPT_PATH=$(realpath "$0")
-DIRNAME=$(dirname "$SCRIPT_PATH")
+SCRIPT_DIRNAME=$(dirname "$SCRIPT_PATH")
 
-if [ -L "$SETTINGS_PATH" ]; then
-    echo "Settings file already exists as a symlink"
-    exit 0
-fi
+if [ ! -L "$SETTINGS_PATH" ]; then
 
-# echo "$settings_path"
-# echo "$backup_path"
-echo "$DIRNAME"
-
-# exit 0
-
-if [ -f "$SETTINGS_PATH" ]; then
-    if [ -f "backup_path" ] ; then
-        echo "Both settings and backup already exists. Exiting"
-        exit 1
+    if [ -f "$SETTINGS_PATH" ]; then
+        if [ -f "backup_path" ] ; then
+            echo "Both settings and backup already exists. Exiting"
+            exit 1
+        fi
+        echo "Settings file already exists, renaming it to $BACKUP_SETTINGS_PATH"
+        mv "$SETTINGS_PATH" "$BACKUP_SETTINGS_PATH"
     fi
-    echo "Settings file already exists, renaming it to $BACKUP_PATH"
-    mv "$SETTINGS_PATH" "$BACKUP_PATH"
+
+    echo "Symlinking $SCRIPT_DIRNAME/settings.json to $SETTINGS_PATH"
+    ln -s "$SCRIPT_DIRNAME/settings.json" "$SETTINGS_PATH" | exit 1
+    echo "Symlinking settings success"
+else
+    echo "Settings symlink already exists (not checked if it is valid though)"
 fi
 
-echo "Symlinking $DIRNAME/settings.json to $SETTINGS_PATH"
-ln -s "$DIRNAME/settings.json" "$SETTINGS_PATH" | exit 1
-echo "Symlink success"
+if [ ! -L "$KEYBINDINGS_PATH" ]; then
+
+    if [ -f "$KEYBINDINGS_PATH" ]; then
+        if [ -f "backup_path" ] ; then
+            echo "Both settings and backup already exists. Exiting"
+            exit 1
+        fi
+        echo "Settings file already exists, renaming it to $BACKUP_KEYBINDINGS_PATH"
+        mv "$KEYBINDINGS_PATH" "$BACKUP_KEYBINDINGS_PATH"
+    fi
+
+    echo "Symlinking $SCRIPT_DIRNAME/keybindings.json to $KEYBINDINGS_PATH"
+    ln -s "$SCRIPT_DIRNAME/keybindings.json" "$KEYBINDINGS_PATH" | exit 1
+    echo "Symlinking keybindings success"
+else
+    echo "Keybindings symlink already exists (not checked if it is valid though)"
+fi
+
+
+
